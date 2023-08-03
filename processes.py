d1dca1c58a430b581f155c80c5f16dea276f9b46
Guilherme Zanus Moraes <guilherme.zanus@gmail.com>
2023-08-03 20:43:09 -0300

    Update processes.py

--- processes.py
+++ processes.py
@@ -71,8 +71,8 @@ class Actions:
         time.sleep(5)
 
     def save_workbook(self) -> None:
-        # self.excel_file.save_workbook("news.xlsx")
-        self.excel_file.save_workbook()
+        self.excel_file.save_workbook("/output/news.xlsx")
+        # self.excel_file.save_workbook()
 
     def create_workbook(self) -> None:
         # self.excel_file.create_workbook()
