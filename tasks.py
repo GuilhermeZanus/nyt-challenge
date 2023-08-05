25f884d09357b642f978e2c5e59aeacb110607a0
Guilherme Zanus Moraes <guilherme.zanus@gmail.com>
2023-08-05 11:32:09 -0300

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -47,7 +47,8 @@ class NyTimesAutomation:
 
         # self.work_items.get_work_item_payload()
         # for variable, value in variables.items():
-        sections = str(self.wi.set_work_item_variable("sections",list(variables.values())[0]))
+        # sections = str(self.wi.set_work_item_variable("sections",list(variables.values())[0]))
+        sections = str(self.wi.get_work_item_variable(list(variables.values())[0]))
         print(list(variables.values())[0])
         print(sections)
         # sections = self.wi.get_work_item_variable(variables.values[0])
